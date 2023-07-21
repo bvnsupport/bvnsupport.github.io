99d264b9a76769f5e8aeecb201d670d9c3d62a37
bvnsupport <bvnjb999@gmail.com>
2023-07-21 15:38:16 +0700

    -

--- update.sh
+++ update.sh
@@ -55,7 +55,7 @@ apt-ftparchive packages ./debs > ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
 echo "------------------"
 echo "Building Release...."
-printf "Origin: ʙᴠɴ ʀᴇᴘᴏsɪᴛᴏʀʏ\nLabel: Biện Văn Nhật\nSuite: stable\nVersion: 1.1\nCodename: ios\nArchitectures: iphoneos-arm iphoneos-arm64\nComponents: main\nDescription: Apple-Support\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: 𝔹𝕍ℕ ℝ𝔼ℙ𝕆𝕊𝕀𝕋𝕆ℝ𝕐\nLabel: Biện Văn Nhật\nSuite: stable\nVersion: 1.1\nCodename: ios\nArchitectures: iphoneos-arm iphoneos-arm64\nComponents: main\nDescription: Apple-Support\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 
 echo "------------------"
 echo "Done!"
